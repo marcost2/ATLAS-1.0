bc88ff2d1e2ff98ed8af9b12355868e58ec9dd9c
Florencia <florenciareckziegel@gmail.com>
2023-04-25 14:23:23 -0300

    dimension UNLIMITED de time en archivo netcdf para soportar meteos mas grandes

--- Sources/readgrn.f90
+++ Sources/readgrn.f90
@@ -145,11 +145,6 @@
             end do
             close (98)
          end if
-
-         if (my_id .ne. 0) then
-            open (90, file=TRIM(fgrn), status='old', err=110)
-            close (90, status='delete')
-         end if
          !
       end do
       !
